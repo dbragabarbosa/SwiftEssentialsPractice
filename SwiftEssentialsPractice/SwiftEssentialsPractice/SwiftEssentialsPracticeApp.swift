e8ff67737699e781d25024f8dae0c26f590dc44f
Daniel Braga Barbosa <dbragabarbosa@gmail.com>
2025-11-16 15:48:57 -0300

    feat: start project

---
+++ SwiftEssentialsPractice/SwiftEssentialsPractice/SwiftEssentialsPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftEssentialsPracticeApp.swift
+//  SwiftEssentialsPractice
+//
+//  Created by Daniel Braga Barbosa on 16/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftEssentialsPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
